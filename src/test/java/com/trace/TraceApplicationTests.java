34ee4d1ba67f4126e875222def3d36139ea96a73
Sayam Gada <sayamgada2005@gmail.com>
2026-08-24 14:52:09 +0530

    chore: establish development and test foundation

--- src/test/java/com/trace/TraceApplicationTests.java
+++ src/test/java/com/trace/TraceApplicationTests.java
@@ -2,12 +2,14 @@ package com.trace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TraceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
